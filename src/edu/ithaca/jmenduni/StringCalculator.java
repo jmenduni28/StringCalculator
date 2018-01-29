b4d084edc8831530f1db6364440b652dca5beaa6
Joe Menduni <jmenduni@ithaca.edu>
2018-01-29 09:37:45 -0500

    Added test to check for an empty string.

--- src/edu/ithaca/jmenduni/StringCalculator.java
+++ src/edu/ithaca/jmenduni/StringCalculator.java
@@ -5,7 +5,7 @@ package edu.ithaca.jmenduni;
  */
 public class StringCalculator {
 
-  public int add(String numbers) {
+  public static int add(String numbers) {
     return 1;
   }
 }
--- test/edu/ithaca/jmenduni/StringCalculatorTest.java
+++ test/edu/ithaca/jmenduni/StringCalculatorTest.java
@@ -2,12 +2,19 @@ package edu.ithaca.jmenduni;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * Created by joemenduni on 1/26/18.
  */
 class StringCalculatorTest {
+
+  /**
+   * Tests the function to add numbers in a string
+   */
   @Test
   void addTest() {
+    assertEquals(StringCalculator.add(""), 0);
   }
 
 }
