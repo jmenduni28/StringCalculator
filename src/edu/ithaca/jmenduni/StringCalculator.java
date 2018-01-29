7cf51dffcd2c6e490467ca06fb3b3bcde1bb9174
Joe Menduni <jmenduni@ithaca.edu>
2018-01-29 09:29:57 -0500

    First commit

---
+++ src/edu/ithaca/jmenduni/StringCalculator.java
@@ -0,0 +1,11 @@
+package edu.ithaca.jmenduni;
+
+/**
+ * Created by joemenduni on 1/26/18.
+ */
+public class StringCalculator {
+
+  public int add(String numbers) {
+    return 1;
+  }
+}
---
+++ test/edu/ithaca/jmenduni/StringCalculatorTest.java
@@ -0,0 +1,13 @@
+package edu.ithaca.jmenduni;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by joemenduni on 1/26/18.
+ */
+class StringCalculatorTest {
+  @Test
+  void addTest() {
+  }
+
+}
