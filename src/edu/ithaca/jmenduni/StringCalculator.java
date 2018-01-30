bca74f60b482923629e4d7e13b0ba91ea01862b1
Joe Menduni <jmenduni@ithaca.edu>
2018-01-29 20:11:02 -0500

    Implemented using a newline as a delimiter.

--- src/edu/ithaca/jmenduni/StringCalculator.java
+++ src/edu/ithaca/jmenduni/StringCalculator.java
@@ -7,14 +7,15 @@ public class StringCalculator {
 
   /**
    * Adds numbers in a string
-   * @param numbers - list of numbers, up to 2, which should be separated by commas
+   * @param numbers - list of numbers, up to 2, which should be separated by commas or newlines
    * @return sum of numbers in the input list, or 0 if the list is empty
    */
   public int add(String numbers) {
     if (numbers == null || numbers.isEmpty()) {
       return 0;
     }
-    else if (numbers.contains(",")) {
+    else if (numbers.contains(",") || numbers.contains("\n")) {
+      numbers = numbers.replaceAll("\n", ",");
       String[] strArray = numbers.split(",");
       int sum = 0;
       for (String str: strArray) {
