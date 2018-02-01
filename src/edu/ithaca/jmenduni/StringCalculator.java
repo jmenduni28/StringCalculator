3a80bb13dcb9c8b89b6b45d7129cb69cc243db59
Joe Menduni <jmenduni@ithaca.edu>
2018-02-01 09:51:54 -0500

    Removed parts 5-7

--- src/edu/ithaca/jmenduni/StringCalculator.java
+++ src/edu/ithaca/jmenduni/StringCalculator.java
@@ -1,8 +1,5 @@
 package edu.ithaca.jmenduni;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by joemenduni on 1/26/18.
  
@@ -11,53 +8,31 @@ public class StringCalculator {
 
   /**
    * Adds numbers in a string
-   * @param numbers - list of numbers, up to 2, which should be separated by commas, newlines, or a specified custom delimiter
+   * @param numbers - list of numbers, up to 2, which should be separated by commas
    * @return sum of numbers in the input list, or 0 if the list is empty
    */
-  public int add(String numbers) throws IllegalArgumentException {
+  public int add(String numbers) {
     // if the string is empty, return 0
     if (numbers == null || numbers.isEmpty()) {
       return 0;
     }
 
     // gets custom decimeter and replaces it with commas
-      
-    // look up built in functions for string iterator
-    if (numbers.startsWith("//")) {
-      String delimeter = numbers.substring(2).split("\n")[0];
-      numbers = numbers.replace("//" + delimeter + "\n", "");
-      numbers = numbers.replaceAll(delimeter, ",");
-    }
 
     // if there is more than 1 number
       if (numbers.contains(",") || numbers.contains("\n")) {
-      numbers = numbers.replaceAll("\n", ",");
       // splits numbers into list
       String[] strArray = numbers.split(",");
       int sum = 0;
-      // keeps track of whether a negative number was found
-      boolean hasNegative = false;
-      // holds negative numbers to return in exception
-      List<Integer> negativeList = new ArrayList<>();
       for (String str: strArray) {
         int num = Integer.parseInt(str);
-        if (num < 0) {
-          hasNegative = true;
-          negativeList.add(num);
-        }
         sum += num;
-      }
-      if (hasNegative) {
-        throw new IllegalArgumentException("negatives not allowed: " + negativeList.toString());
       }
       return sum;
     }
     // if there is just one number in the string
     else {
       int val = Integer.parseInt(numbers);
-      if (val < 0) {
-        throw new IllegalArgumentException("negatives not allowed: " + val);
-      }
       return val;
     }
   }
--- test/edu/ithaca/jmenduni/StringCalculatorTest.java
+++ test/edu/ithaca/jmenduni/StringCalculatorTest.java
@@ -1,8 +1,8 @@
 package edu.ithaca.jmenduni;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Created by joemenduni on 1/26/18.
@@ -13,7 +13,7 @@ class StringCalculatorTest {
    * Tests the function to add numbers in a string with an empty string
    */
   @Test
-  void testAddEmpty() throws IllegalArgumentException {
+  void testAddEmpty() {
     StringCalculator stringCalculator = new StringCalculator();
     assertEquals(0, stringCalculator.add(null));
     assertEquals(0, stringCalculator.add(""));
@@ -23,30 +23,30 @@ class StringCalculatorTest {
    * Tests the function to add numbers in a string with one number
    */
   @Test
-  void testAddOneNum() throws IllegalArgumentException {
+  void testAddOneNum()  {
     StringCalculator stringCalculator = new StringCalculator();
     assertEquals(0, stringCalculator.add("0"));
     assertEquals(1, stringCalculator.add("1"));
     assertEquals(5, stringCalculator.add("5"));
     assertEquals(10, stringCalculator.add("10"));
     assertEquals(1000, stringCalculator.add("1000"));
-    //assertEquals(-1, stringCalculator.add("-1"));
-    //assertEquals(-10, stringCalculator.add("-10"));
+    assertEquals(-1, stringCalculator.add("-1"));
+    assertEquals(-10, stringCalculator.add("-10"));
   }
 
   /**
    * Tests the function to add numbers in a string with two numbers
    */
   @Test
-  void testAddTwoNums()  throws IllegalArgumentException {
+  void testAddTwoNums() {
     StringCalculator stringCalculator = new StringCalculator();
     assertEquals(0, stringCalculator.add("0,0"));
     assertEquals(1, stringCalculator.add("1,0"));
     assertEquals(2, stringCalculator.add("1,1"));
     assertEquals(15, stringCalculator.add("5,10"));
     assertEquals(15, stringCalculator.add("10,5"));
-    //assertEquals(-5, stringCalculator.add("-7,2"));
-    //assertEquals(-10, stringCalculator.add("-5,-5"));
+    assertEquals(-5, stringCalculator.add("-7,2"));
+    assertEquals(-10, stringCalculator.add("-5,-5"));
 
   }
 
@@ -54,55 +54,15 @@ class StringCalculatorTest {
    * Tests the function to add numbers in a string with an unknown amount of numbers
    */
   @Test
-  void testAddUnknownNums() throws IllegalArgumentException {
+  void testAddUnknownNums() {
     StringCalculator stringCalculator = new StringCalculator();
     assertEquals(6, stringCalculator.add("1,2,3"));
     assertEquals(15, stringCalculator.add("1,2,3,4,5"));
     assertEquals(29, stringCalculator.add("1,2,3,4,5,6,8"));
     assertEquals(45, stringCalculator.add("1,2,3,4,5,6,8,9,7"));
-    //assertEquals(-10, stringCalculator.add("-2,-3,-5"));
-    //assertEquals(-7, stringCalculator.add("-1,-2,-3,4,-5"));
+    assertEquals(-10, stringCalculator.add("-2,-3,-5"));
+    assertEquals(-7, stringCalculator.add("-1,-2,-3,4,-5"));
   }
 
-  /**
-   * Tests the function to add numbers in a string with commas or newlines as delimiters
-   */
-  @Test
-  void testAddNewLines()  throws IllegalArgumentException {
-    StringCalculator stringCalculator = new StringCalculator();
-    assertEquals(3, stringCalculator.add("1\n2"));
-    assertEquals(6, stringCalculator.add("1\n2,3"));
-    assertEquals(6, stringCalculator.add("1\n2\n3"));
-  }
-
-  /**
-   * Tests the function to add numbers in a string with a custom delimiter
-   */
-  @Test
-  void testAddCustomDelimiter() throws IllegalArgumentException {
-    StringCalculator stringCalculator = new StringCalculator();
-    assertEquals(3, stringCalculator.add("//;\n1;2"));
-    assertEquals(10, stringCalculator.add("//%\n1%2%3%4"));
-  }
-
-  /**
-   * Tests the function to add numbers in a string with a negative number, which should throw an exception
-   */
-  @Test
-  void testNegativeException() {
-    final StringCalculator stringCalculator = new StringCalculator();
-    assertThrows(IllegalArgumentException.class,
-            ()->{
-              stringCalculator.add("-1");
-            });
-    assertThrows(IllegalArgumentException.class,
-            ()->{
-              stringCalculator.add("-5, -5");
-            });
-    assertThrows(IllegalArgumentException.class,
-            ()->{
-              stringCalculator.add("1, -5");
-            });
-  }
 
 }
