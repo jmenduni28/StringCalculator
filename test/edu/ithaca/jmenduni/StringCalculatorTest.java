5bd0662ed4156dff65a4a7550b4e319f200ab22c
Joe Menduni <jmenduni@ithaca.edu>
2018-02-01 09:40:40 -0500

    Added tests for negative numbers.

--- test/edu/ithaca/jmenduni/StringCalculatorTest.java
+++ test/edu/ithaca/jmenduni/StringCalculatorTest.java
@@ -30,6 +30,8 @@ class StringCalculatorTest {
     assertEquals(5, stringCalculator.add("5"));
     assertEquals(10, stringCalculator.add("10"));
     assertEquals(1000, stringCalculator.add("1000"));
+    assertEquals(-1, stringCalculator.add("-1"));
+    assertEquals(-10, stringCalculator.add("-10"));
   }
 
   /**
@@ -43,6 +45,9 @@ class StringCalculatorTest {
     assertEquals(2, stringCalculator.add("1,1"));
     assertEquals(15, stringCalculator.add("5,10"));
     assertEquals(15, stringCalculator.add("10,5"));
+    assertEquals(-5, stringCalculator.add("-7,2"));
+    assertEquals(-10, stringCalculator.add("-5,-5"));
+
   }
 
   /**
@@ -55,6 +60,8 @@ class StringCalculatorTest {
     assertEquals(15, stringCalculator.add("1,2,3,4,5"));
     assertEquals(29, stringCalculator.add("1,2,3,4,5,6,8"));
     assertEquals(45, stringCalculator.add("1,2,3,4,5,6,8,9,7"));
+    assertEquals(-10, stringCalculator.add("-2,-3,-5"));
+    assertEquals(-7, stringCalculator.add("-1,-2,-3,4,-5"));
   }
 
   /**
